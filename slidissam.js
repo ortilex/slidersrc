6384b1a4547eab6d1f42c8af223ab2104452ea06
ortilex <33230389+ortilex@users.noreply.github.com>
2018-01-18 13:13:08 +0100

    Create slidissam.js

---
+++ slidissam.js
@@ -0,0 +1,11 @@
+ $( "#slider-range" ).slider({
+											range: true,
+											min: 0,
+											max: 20000,
+											values: [0, 16000 ],
+											slide: function( event, ui ) {  $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+											}
+								 });
+								$( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) + " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+
+								});
